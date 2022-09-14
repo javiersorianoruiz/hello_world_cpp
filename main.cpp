c4bbfebedace1cc0f32357622c588391948788cf
Bit-Seq <victor_loeches@hotmail.com>
2022-09-14 14:00:01 +0200

    First commit in branch 2 done from User 2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main(int ac, char** av) { 
     //Hello World
     std::cout << "Hello World User 1 y User 2!" << std::endl;
+    std::cout << "Change in branch 2 done from User 2" << std::endl;
 
     return 0; 
 }
