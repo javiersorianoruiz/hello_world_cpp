f516db1e713f8d5bed5d1074a0f0409f453d8a3f
Bit-Seq <victor_loeches@hotmail.com>
2022-09-15 09:15:15 +0200

    Commit of a change in a fork from User 2

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int ac, char** av) {
     std::cout << "Hello World User 1 y User 2!" << std::endl;
     std::cout << "Change in branch 2 done from User 2" << std::endl;
     std::cout << "Change in branch 1 done from User 1" << std::endl;
+    std::cout << "Change in fork from User 2" << std::endl;
 
     return 0; 
 }
