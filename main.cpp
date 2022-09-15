ebc1ce8ce70d7cc005b6397af9a5cfdb57b67431
Bit-Seq <victor_loeches@hotmail.com>
2022-09-15 09:26:17 +0200

    Commit of a second change in a fork from User 2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int ac, char** av) {
     std::cout << "Change in branch 2 done from User 2" << std::endl;
     std::cout << "Change in branch 1 done from User 1" << std::endl;
     std::cout << "Change in fork from User 2" << std::endl;
+    std::cout << "Change 2 in fork from User 2" << std::endl;
 
     return 0; 
 }
