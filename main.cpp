b5c3a303b850ea8cf258aca1c9e6cae5c69c715f
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-11-28 10:06:25 +0100

    nuevo cout

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 int main(int ac, char** av) { 
     //Hello World
+    std::cout << "cambio hecho en otro repositorio local y subido al remoto" << std::endl; //Nueva linea no conocida por el otro repositorio
+
+
     std::cout << "Hello World User 1 y User 2!" << std::endl;
     std::cout << "Change in branch 2 done from User 2" << std::endl;
     std::cout << "Change in branch 1 done from User 1" << std::endl;
