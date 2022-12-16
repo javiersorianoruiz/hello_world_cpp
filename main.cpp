982109b96ca520d12f07152c786ee86199f70dfa
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-12-16 14:04:07 +0100

    cambio en la rama main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ int main(int ac, char** av) {
     //Hello World
     
     std::cout << "Hello World User 1 y User 2!" << std::endl;
+
+    std::cout << "cambio en la rama main" << std::endl;
     
     return 0; 
 }
