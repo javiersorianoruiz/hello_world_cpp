3163c843115f2f5536c90a465190894e6475d1dd
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-12-16 14:10:18 +0100

    segundo cambio en nueva_rama tras tag v1.0.0

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int ac, char** av) {
     std::cout << "Hello World User 1 y User 2!" << std::endl;
     
     std::cout << "primer cambio en nueva_rama" << std::endl;
+
+    std::cout << "segundo cambio en nueva_rama tras tag v1.0.0" << std::endl;
     
     return 0; 
 }
