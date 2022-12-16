d676df0224ebaf77d094f7b3022d0ec49d997b66
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-12-16 14:17:22 +0100

    cambio en nueva_rama2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int ac, char** av) {
     std::cout << "Hello World User 1 y User 2!" << std::endl;
     
     std::cout << "primer cambio en nueva_rama" << std::endl;
+
+    std::cout << "cambio en nueva_rama2" << std::endl;
     
     return 0; 
 }
