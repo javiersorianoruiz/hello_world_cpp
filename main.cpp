afd8d00fb7cfd5e69c017797381d78c9b0da091f
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-12-16 13:56:27 +0100

    main limpio

--- main.cpp
+++ main.cpp
@@ -4,14 +4,6 @@ int main(int ac, char** av) {
     //Hello World
     
     std::cout << "Hello World User 1 y User 2!" << std::endl;
-    std::cout << "primer cambio ramas_pruebas_tags" << std::endl;
-    std::cout << "cambio en el main sin venir de ninguna rama" << std::endl;
-
-    std::cout << "segundo cambio ramas_pruebas_tags" << std::endl;
-
-    std::cout << "tercer cambio ramas_pruebas_tags tras el tag v.1.0.0" << std::endl;
-
-    std::cout << "cuarto cambio ramas2_pruebas_tags" << std::endl;
-
+    
     return 0; 
 }
