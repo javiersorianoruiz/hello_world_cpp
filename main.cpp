64d44d07941cea1df0d91be2619d492ee7df9cd6
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2023-01-10 13:53:45 +0100

    main limpio rama Feature_EVC_MonoChannel

--- main.cpp
+++ main.cpp
@@ -3,15 +3,8 @@
 int main(int ac, char** av) { 
     //Hello World
     
-    std::cout << "Hello World User 1 y User 2!" << std::endl;
+    std::cout << "Hello World Rama: Feature_EVC_MonoChannel" << std::endl;
 
-    std::cout << "cambio en la rama main" << std::endl;
-    
-    std::cout << "primer cambio en nueva_rama" << std::endl;
-
-    std::cout << "segundo cambio en nueva_rama tras tag v1.0.0" << std::endl;
-
-    std::cout << "cambio en nueva_rama2" << std::endl;
     
     return 0; 
 }
