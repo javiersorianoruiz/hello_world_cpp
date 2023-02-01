a6859b45141c1bea46e5ef639dc1fdc816c53889
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2023-02-01 14:14:29 +0100

    prueba cambio rama

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int ac, char** av) { 
     //Hello World
-    
+    std::cout << "cambio antes de crear la rama_prueba1" << std::endl; 
     std::cout << "Hello World Rama: Feature_EVC_MonoChannel" << std::endl;
 
     
